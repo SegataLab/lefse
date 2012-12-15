bb46ccc5698751c18b4659aa033037467f5b3b25
nsegata <nsegata@localhost>
2012-12-15 17:26:44 +0100

    Small graphical bug in plot_res.py fixed

--- format_input.py
+++ format_input.py
@@ -258,9 +258,9 @@ if  __name__ == '__main__':
 
 	if params['output_table']:
 		with open( params['output_table'], "w") as outf: 
-			if 'class' in cls: outf.write( "\t".join(["class"]+cls['class']) + "\n" )
+			if 'class' in cls: outf.write( "\t".join(list(["class"])+list(cls['class'])) + "\n" )
 			if 'subclass' in cls: outf.write( "\t".join(["subclass"]+cls['subclass']) + "\n" )
-			if 'subject' in cls: outf.write( "\t".join(i["subject"]+cls['subject'])  + "\n" )
+			if 'subject' in cls: outf.write( "\t".join(["subject"]+cls['subject'])  + "\n" )
 			for k,v in out['feats'].items(): outf.write( "\t".join([k]+[str(vv) for vv in v]) + "\n" )
 
 	with open(params['output_file'], 'wb') as back_file:
